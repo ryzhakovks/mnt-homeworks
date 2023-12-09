1bb2ef9ec6e0bc552c7a7a3d15a7c3d89c63a183
Ваше Имя <kirill.ryzhakov@megafon.ru>
2023-12-09 21:01:58 +0500

    add

--- 09-ci-03-cicd/example/fail.py
+++ 09-ci-03-cicd/example/fail.py
@@ -1,11 +1,10 @@
-def increment(index):
-    index =+ 1
-    return index
+def increment(index_lo):
+    index_lo = index_lo + 1
+    return index_lo
 def get_square(numb):
     return numb*numb
 def print_numb(numb):
     print("Number is {}".format(numb))
-    pass
 
 index = 0
 while (index < 10):
